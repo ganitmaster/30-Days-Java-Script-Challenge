e59e3c950906e31f2098563af1b5c1a40564c7d7
ganitmaster <somnath.tiwari0902@gmail.com>
2024-07-21 17:01:37 +0000

    DAY -04: LOOPS

---
+++ DAY_04/loops.js
@@ -0,0 +1,35 @@
+// DAY : 04
+// Activity 1: for loop
+// Task 1: Write a program to print numbers from 1 to 10 using for loop
+for(let i=1;i<=10;i++){
+    console.log(i)
+}
+// Task 2 : Write a program to print the multiplication table of 5 using for loop
+let num = 5
+for(let i=1;i<=10;i++){
+    console.log(i*num)
+}
+
+// Activity 2: while loop
+// Task 3: Write a program to calculate sum of nimbers from 1 to 10 using a while loop
+let sum=0
+let i=11
+while(i--){
+    sum+=i
+}
+console.log(sum)
+
+// Task 4: Write a program to print numbers from 10 to 1 using while loop
+let j=10
+while(j>=1){
+    console.log(j)
+    j--
+}
+
+// Activity 3: Do - While loop
+// Task 5: Write a program to print numbers from 1 to 5 using do - while loop
+let n =1;
+do{
+    n += 1;
+    console.log(n)
+}while(n<5)
