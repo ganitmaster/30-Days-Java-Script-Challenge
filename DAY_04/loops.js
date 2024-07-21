b6d59c2a0fa10b716163518cca18f2687c255f58
ganitmaster <somnath.tiwari0902@gmail.com>
2024-07-21 17:56:08 +0000

    DAY 04 : LOOPS

--- DAY_04/loops.js
+++ DAY_04/loops.js
@@ -21,15 +21,58 @@ console.log(sum)
 
 // Task 4: Write a program to print numbers from 10 to 1 using while loop
 let j=10
-while(j>=1){
+while(j>0){
     console.log(j)
     j--
 }
 
 // Activity 3: Do - While loop
 // Task 5: Write a program to print numbers from 1 to 5 using do - while loop
-let n =1;
+let n =0;
 do{
     n += 1;
     console.log(n)
 }while(n<5)
+
+// Task 6: Write a program to calculate factorial of number using do - while loop
+let m = 5
+let fact = 1
+do{
+    fact = fact*m;
+    m--;
+}while(m>0)
+console.log(fact)
+
+// Activity 4: Nested Loops
+// Task 7: Write a program to print a pattern using nested for loops
+/*  x
+    xx
+    xxx
+    xxxx
+    xxxxx
+*/
+let pattern = ""
+for(let i=0;i<5;i++)
+{
+    for(let j=0;j<=i;j++)
+    {
+        pattern += "*";
+    }
+    pattern+="\n"
+}
+console.log(pattern)
+
+// Activity 5: Loop Control Statements
+// Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using continue statement
+for(let i=1;i<=10;i++){
+    if(i==5){continue;}
+    console.log(i);
+}
+
+// Task 9: Write a program to print numbers from 1 to 10, but stop the loop when number is 7 using brreak stmt.
+for(let i=1;i<=10;i++){
+    if(i==7){
+        break;
+    }
+    console.log(i);
+}
